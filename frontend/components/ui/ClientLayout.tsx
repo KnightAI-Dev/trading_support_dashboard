e46a0d797e7112486c812de4534b614d2559d501
rianmatos2412-code <rianmatos2412@gmail.com>
2025-11-24 12:58:46 -0600

    Remove the loading bar

--- frontend/components/ui/ClientLayout.tsx
+++ frontend/components/ui/ClientLayout.tsx
@@ -1,12 +1,10 @@
 "use client";
 
-import { PageLoadingBar } from "./PageLoadingBar";
 import { Navigation } from "./Navigation";
 
 export function ClientLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <PageLoadingBar />
       <Navigation />
       {children}
     </>
